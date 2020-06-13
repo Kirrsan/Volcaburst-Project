167eb13ab26a354dac4c26b9c11a1ff5011b0961
Grégoire <gpierillas@gmail.com>
2020-06-13 17:41:29 +0200

    Proto water/looseCondition setup

---
+++ Volcaburst/Assets/Scripts/GameStateEnum.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateEnum : MonoBehaviour
+{
+    public enum GAMESTATE
+    {
+        Menu,
+        InGameTerraforming,
+        InGameFluid,
+        Win,
+        Loose
+    }
+
+    public static GAMESTATE gameStateEnum;
+
+}
---
+++ Volcaburst/Assets/Scripts/PhaseManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaseManager : MonoBehaviour
+{
+
+    public float firstPhaseTimer;
+    private float firstPhaseTimerMax;
+
+    public GameObject lavaSpawnObject;
+    public GameStateEnum.GAMESTATE startGameState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPhaseTimerMax = firstPhaseTimer;
+        GameStateEnum.gameStateEnum = startGameState;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PhaseBehaviour();
+    }
+
+    private void PhaseBehaviour()
+    {
+        if(GameStateEnum.gameStateEnum == GameStateEnum.GAMESTATE.InGameTerraforming)
+        {
+            FirstPhase();
+        }
+        else if (GameStateEnum.gameStateEnum == GameStateEnum.GAMESTATE.InGameFluid)
+        {
+            SecondPhase();
+        }
+        else if (GameStateEnum.gameStateEnum == GameStateEnum.GAMESTATE.Loose)
+        {
+            LooseState();
+        }
+    }
+
+    private void FirstPhase()
+    {
+        firstPhaseTimer -= Time.deltaTime;
+        if (firstPhaseTimer <= 0)
+        {
+            GameStateEnum.gameStateEnum = GameStateEnum.GAMESTATE.InGameFluid;
+            firstPhaseTimer = firstPhaseTimerMax; 
+        }
+    }
+
+    private void SecondPhase()
+    {
+        lavaSpawnObject.SetActive(true);
+    }
+
+    private void LooseState()
+    {
+        //lavaSpawnObject.SetActive(false);
+        //display defeat screen
+    }
+
+    //called on event OnParticleCollisions
+    public void SwitchToDefeatPhase(GameObject LooseObject)
+    {
+        GameStateEnum.gameStateEnum = GameStateEnum.GAMESTATE.Loose;
+        LooseObject.SetActive(false);
+    }
+}
